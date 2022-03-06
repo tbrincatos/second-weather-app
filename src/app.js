1df3a676d92cab2e03315d12319bf7b6b16966de
tbrincatos <88725546+tbrincatos@users.noreply.github.com>
2022-03-06 16:32:53 +0100

    JS Calculation from Celsius to Fahrenheit + API call with search engine data
    Js : functions for event upon click of C or F and calculation to and from;
    bug: if you leave it on fahrenheit and then type something in the search engine, the fahrenheit stays on and the calculation upon click is false... to modify
    api call on the search engine form with 3 api calls to update : city, temperature and description (which isn't great)

--- src/app.js
+++ src/app.js
@@ -36,11 +36,15 @@ function injectDate(date) {
 
 injectDate(new Date());
 
-function handleData(response) {
+function displayCity(response) {
   document.querySelector("#city-name").innerHTML = response.data.name;
+}
+function displayCelsiusTemp(response) {
   document.querySelector("#current-temp-number").innerHTML = Math.round(
     response.data.main.temp
   );
+}
+function displayDescription(response) {
   document.querySelector("#weather-description").innerHTML =
     response.data.weather[0].description;
 }
@@ -52,7 +56,9 @@ function handleCoords(position) {
   let unit = `metric`;
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=${unit}&appid=${apiKey}`;
 
-  axios.get(apiUrl).then(handleData);
+  axios.get(apiUrl).then(displayCity);
+  axios.get(apiUrl).then(displayCelsiusTemp);
+  axios.get(apiUrl).then(displayDescription);
 }
 function getCoords(event) {
   event.preventDefault();
@@ -61,3 +67,63 @@ function getCoords(event) {
 
 let currentWeather = document.querySelector("#current-location-weather");
 currentWeather.addEventListener("click", getCoords);
+
+function calculateFahrenheit() {
+  document.querySelector("#fahrenheit").innerHTML = `<strong>°F</strong>`;
+  document.querySelector(
+    "#celsius"
+  ).innerHTML = ` <a href="#" id="convert-to-celsius">°C </a>`;
+  let backToCelsius = document.querySelector("#convert-to-celsius");
+  backToCelsius.addEventListener("click", convertBackToCelsius);
+  let celsiusTemp = document.querySelector("#current-temp-number").innerHTML;
+  let celsiusTempNumber = Number(celsiusTemp);
+  document.querySelector("#current-temp-number").innerHTML = Math.round(
+    (celsiusTempNumber * 9) / 5 + 32
+  );
+}
+function displayFahrenheitTemp(event) {
+  event.preventDefault();
+  calculateFahrenheit();
+}
+let fahrenheit = document.querySelector("#convert-to-fahrenheit");
+fahrenheit.addEventListener("click", displayFahrenheitTemp);
+
+function calculateCelsius() {
+  document.querySelector("#celsius").innerHTML = `<strong>°C</strong>`;
+  document.querySelector(
+    "#fahrenheit"
+  ).innerHTML = `<a href="#" id="convert-to-fahrenheit">°F</a>`;
+  let fahrenheit = document.querySelector("#convert-to-fahrenheit");
+  fahrenheit.addEventListener("click", displayFahrenheitTemp);
+  let fahTemp = document.querySelector("#current-temp-number").innerHTML;
+  let fahTempNumber = Number(fahTemp);
+  document.querySelector("#current-temp-number").innerHTML = Math.round(
+    ((fahTempNumber - 32) * 5) / 9
+  );
+}
+function convertBackToCelsius(event) {
+  event.preventDefault();
+  calculateCelsius();
+}
+function searchCity(city) {
+  let apiKey = `62f780f73f5ee00aa0f4d27f32e096c2`;
+  let unit = `metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${unit}&appid=${apiKey}`;
+
+  axios.get(apiUrl).then(displayCity);
+  axios.get(apiUrl).then(displayCelsiusTemp);
+  axios.get(apiUrl).then(displayDescription);
+}
+function handleSubmit(event) {
+  event.preventDefault();
+  let userInput = document.querySelector("#search-input");
+  let input = userInput.value;
+  input = input.trim();
+  let inputRest = input.toLowerCase();
+  let inputFirst = input.charAt(0).toUpperCase();
+  input = inputFirst + inputRest.substr(1);
+
+  searchCity(input);
+}
+let searchEngineForm = document.querySelector("#search-engine-form");
+searchEngineForm.addEventListener("submit", handleSubmit);
